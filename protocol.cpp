6d161d296a8bc3614e7f39ed033a98d5aeb68019
SynchrotronCoinDev <44231207+SynchrotronCoinDev@users.noreply.github.com>
2019-01-01 15:36:53 +0100

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0x70, 0x35, 0x22, 0x05 };
+unsigned char pchMessageStart[4] = { 0x53, 0xea, 0x84, 0xbc };
 
 CMessageHeader::CMessageHeader()
 {
